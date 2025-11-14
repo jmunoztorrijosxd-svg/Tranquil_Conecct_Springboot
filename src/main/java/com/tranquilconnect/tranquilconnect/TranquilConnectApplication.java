43547366629de0b160488e7ac4c45f52da3f6a53
Julian Muñoz <jmunoztorrijosxd@gmail.com>
2025-11-14 17:46:41 -0500

    Primer commit de Tranquil Connect

---
+++ src/main/java/com/tranquilconnect/tranquilconnect/TranquilConnectApplication.java
@@ -0,0 +1,13 @@
+package com.tranquilconnect.tranquilconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranquilConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranquilConnectApplication.class, args);
+	}
+
+}
