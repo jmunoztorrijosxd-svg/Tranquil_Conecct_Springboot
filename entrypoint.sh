b3076afbd9920633e77b6f28c70a82b9c1c9aecc
jmunoztorrijosxd-svg <jmunoztorrijosxd@gmail.com>
2026-06-28 11:08:04 -0500

    Forzar conexión DB mediante argumentos en entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # 1. Iniciar Spring Boot en segundo plano (escuchando en 8090)
-java -jar -Dspring.profiles.active=prod -Dserver.port=8090 /app/app.jar &
+# Cambia tu línea de ejecución de java por esta:
+java -Dspring.datasource.url="jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_NAME}?useSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true" \
+     -Dspring.datasource.username="${DB_USER}" \
+     -Dspring.datasource.password="${DB_PASSWORD}" \
+     -jar /app/app.jar &
 
 # 2. Iniciar Django en segundo plano (escuchando en 127.0.0.1:8000)
 # Es vital usar 127.0.0.1 en lugar de 0.0.0.0 para que solo Nginx acceda
